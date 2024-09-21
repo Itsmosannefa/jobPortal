143bbf1ccc33538b23fb04b620b5a60815f46d66
Itsmosannefa <khanammosannefa@gmail.com>
2024-09-21 19:12:40 +0530

    JobDescription

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,11 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 // import Navbar from './components/shared/Navbar'
 import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
-// import Home from './components/Home'
+import Home from './components/Home'
 // import Jobs from './components/Jobs'
 // import Browse from './components/Browse'
 import Profile from './components/Profile'
-// import JobDescription from './components/JobDescription'
+import JobDescription from './components/JobDescription'
 // import Companies from './components/admin/Companies'
 // import CompanyCreate from './components/admin/CompanyCreate'
 // import CompanySetup from './components/admin/CompanySetup'
@@ -29,14 +29,14 @@ const appRouter = createBrowserRouter([
     path: '/signup',
     element: <Signup />
   },
-  // {
-  //   path: "/jobs",
-  //   element: <Jobs />
-  // },
-  // {
-  //   path: "/description/:id",
-  //   element: <JobDescription />
-  // },
+  {
+    path: "/jobs",
+    element: <Jobs />
+  },
+  {
+    path: "/description/:id",
+    element: <JobDescription />
+  },
   // {
   //   path: "/browse",
   //   element: <Browse />
