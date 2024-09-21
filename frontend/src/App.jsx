ba6000fdf0a2e1ff0bfb94fe14636fb17593b677
Parthivthadani <thadaniparthiv2@gmail.com>
2024-09-22 02:11:36 +0530

    added css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,8 @@ import Navbar from './components/shared/Navbar'
 import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
 import Home from './components/Home'
-// import Jobs from './components/Jobs'
-// import Browse from './components/Browse'
+ import Jobs from './components/Jobs'
+import Browse from './components/Browse'
 import Profile from './components/Profile'
 import JobDescription from './components/JobDescription'
 // import Companies from './components/admin/Companies'
@@ -46,30 +46,30 @@ const appRouter = createBrowserRouter([
     element: <Profile />
   },
   // admin ke liye yha se start hoga
-  {
-    path:"/admin/companies",
-    element: <ProtectedRoute><Companies/></ProtectedRoute>
-  },
-  {
-    path:"/admin/companies/create",
-    element: <ProtectedRoute><CompanyCreate/></ProtectedRoute> 
-  },
-  {
-    path:"/admin/companies/:id",
-    element:<ProtectedRoute><CompanySetup/></ProtectedRoute> 
-  },
-  {
-    path:"/admin/jobs",
-    element:<ProtectedRoute><AdminJobs/></ProtectedRoute> 
-  },
-  {
-    path:"/admin/jobs/create",
-    element:<ProtectedRoute><PostJob/></ProtectedRoute> 
-  },
-  {
-    path:"/admin/jobs/:id/applicants",
-    element:<ProtectedRoute><Applicants/></ProtectedRoute> 
-  },
+  // {
+  //   path:"/admin/companies",
+  //   element: <ProtectedRoute><Companies/></ProtectedRoute>
+  // },
+  // {
+  //   path:"/admin/companies/create",
+  //   element: <ProtectedRoute><CompanyCreate/></ProtectedRoute> 
+  // },
+  // {
+  //   path:"/admin/companies/:id",
+  //   element:<ProtectedRoute><CompanySetup/></ProtectedRoute> 
+  // },
+  // {
+  //   path:"/admin/jobs",
+  //   element:<ProtectedRoute><AdminJobs/></ProtectedRoute> 
+  // },
+  // {
+  //   path:"/admin/jobs/create",
+  //   element:<ProtectedRoute><PostJob/></ProtectedRoute> 
+  // },
+  // {
+  //   path:"/admin/jobs/:id/applicants",
+  //   element:<ProtectedRoute><Applicants/></ProtectedRoute> 
+  // },
 
 ])
 function App() {
