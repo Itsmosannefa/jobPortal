ef8abfb82d808e096a912aea8d798c2a5e7485f1
Itsmosannefa <khanammosannefa@gmail.com>
2024-09-22 15:14:54 +0530

    PostJobs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ import Companies from './components/admin/Companies'
 import CompanyCreate from './components/admin/CompanyCreate'
 // import CompanySetup from './components/admin/CompanySetup'
 import AdminJobs from "./components/admin/AdminJobs";
-// import PostJob from './components/admin/PostJob'
+import PostJob from './components/admin/PostJob'
 // import Applicants from './components/admin/Applicants'
 // import ProtectedRoute from './components/admin/ProtectedRoute'
 
@@ -62,10 +62,10 @@ const appRouter = createBrowserRouter([
     path:"/admin/jobs",
     element:<ProtectedRoute><AdminJobs/></ProtectedRoute> 
   },
-  // {
-  //   path:"/admin/jobs/create",
-  //   element:<ProtectedRoute><PostJob/></ProtectedRoute> 
-  // },
+  {
+    path:"/admin/jobs/create",
+    element:<ProtectedRoute><PostJob/></ProtectedRoute> 
+  },
   // {
   //   path:"/admin/jobs/:id/applicants",
   //   element:<ProtectedRoute><Applicants/></ProtectedRoute> 
